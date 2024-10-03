059050c6404550d72255bb35a779c7ba357e81d2
actividad-7-arreglos <emiliomejiasolis@gmail.com>
2024-10-02 19:34:18 -0600

    Repositorio 1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[20];
+
+        System.out.println("Introduce 20 números enteros:");
+        for (int i = 0; i < 20; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println("\nNúmero\tCuadrado\tCubo");
+        for (int i = 0; i < 20; i++) {
+            int number = numbers[i];
+            int square = number * number;
+            int cube = number * number * number;
+            System.out.println(number + "\t" + square + "\t\t" + cube);
+        }
+
+        scanner.close();
+    }
+}
